128362682d883d8663ffa04d7072c658dfbd49bc
Dainik <dainik.arthania@gmail.com>
2022-11-16 11:30:11 +0530

    readme file added

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ app.use("/api/users",UserRouter)
 
 
 app.get("/api/random-joke",async (req,res)=>{
-    let joke = await axios.get("https://api.chucknorris.io/jokes/random")
-    console.log()
-    res.json({flag:true,data:joke.data.value})
+    try{
+        let joke = await axios.get("https://api.chucknorris.io/jokes/random")
+        console.log()
+        res.json({flag:true,data:joke.data.value})
+    }
+    catch(e){
+        res.json({flag:false,message:e.message})
+    }
+   
 })
 
 app.listen("5000")
